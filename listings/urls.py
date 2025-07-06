405e19b1e8ea7085b1b607e5b5cad7f35f82c4ef
Geofrey Simiyu <106971868+Geoffy1@users.noreply.github.com>
2025-07-06 23:52:04 +0300

    Create urls.py

---
+++ listings/urls.py
@@ -0,0 +1,8 @@
+# alx_travel_app/listings/urls.py
+
+from django.urls import path
+
+urlpatterns = [
+    # API endpoints for your listings app will be defined here later
+    # Example: path('my-first-listing-api/', views.MyListingView.as_view(), name='my-listing-api'),
+]
